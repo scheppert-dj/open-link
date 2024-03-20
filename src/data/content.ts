a89cdc6e56b9b432c24a9522b8debd6398f460ba
MBiallas <92144111+MBiallas@users.noreply.github.com>
2024-03-20 14:14:45 +1100

    Update content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -43,12 +43,6 @@ const content: Content = {
 		},
 		{
 			image:
-				'https://res.cloudinary.com/dlq8spaji/image/upload/v1709601295/q1ebyey2dogn9zy1agbb.jpg',
-			url: 'https://m.moshtix.com.au/v2/event/elevate/162490',
-			text: 'Human Sense 16.03'
-		},
-		{
-			image:
 				'https://res.cloudinary.com/dlq8spaji/image/upload/v1710211253/r0t355f9zdfrdfefefwq.png',
 			url: 'https://www.hcbears.com.au/event-5610302',
 			text: 'BEARHAIN #2 22.03'
